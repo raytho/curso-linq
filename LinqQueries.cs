c723667a92ab3a9a1b98a6ec82beeb218c9af3cc
Raymundo Solis <pepperray01@gmail.com>
2024-04-03 16:53:07 -0600

    Manejo de datos | Where Method

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -26,5 +26,23 @@ namespace curso_linq
         {
             return booksCollection;
         }
+
+        public IEnumerable<Book> BooksAfter2000()
+        {
+            // Extension method
+            return booksCollection.Where(p => p.PublishedDate.Year >= 2000);
+
+            // Query expresion
+            //return from l in booksCollection where l.PublishedDate.Year >= 2000 select l;
+        }
+
+        public IEnumerable<Book> BigBooksAction()
+        {
+            // Extension method
+            return booksCollection.Where(p => p.PageCount >= 250 && p.Title.Contains("in Action"));
+
+            // Query expresion
+            //return from l in booksCollection where l.PageCount >= 250 && l.Title.Contains("in Action") select l;
+        }
     }
 }
