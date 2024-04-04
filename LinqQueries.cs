50adfc239c207e17a6bf0a95b6fd7b2143a90300
Raymundo Solis <pepperray01@gmail.com>
2024-04-04 17:05:07 -0600

    Manejo de datos | Take and Skip

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -73,5 +73,18 @@ namespace curso_linq
         {
             return booksCollection.Where(p => p.PageCount >= 450).OrderByDescending(p => p.PageCount);
         }
+
+        public IEnumerable<Book> ThreeJavaBooks()
+        {
+            return booksCollection.Where(p => p.Categories.Contains("Java")).OrderByDescending(p => p.PublishedDate).Take(3);
+        }
+
+        public IEnumerable<Book> ThrithandFourthBooks()
+        {
+            return booksCollection
+            .Where(p => p.PageCount > 400)
+            .Take(4)
+            .Skip(2);
+        }
     }
 }
